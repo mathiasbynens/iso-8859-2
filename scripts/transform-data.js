ca4b551ec483d27326d7d23295686cf332be9f8c
Mathias Bynens <mathias@qiwi.be>
2021-09-05 08:36:31 +0200

    Fix `replacement` encoding mode
    Issue: https://github.com/mathiasbynens/windows-1252/issues/8

--- scripts/transform-data.js
+++ scripts/transform-data.js
@@ -58,7 +58,7 @@ fs.writeFileSync(
 // tests/tests.src.js → tests/tests.js
 const TEST_TEMPLATE = fs.readFileSync('./tests/tests.src.mjs', 'utf8');
 const createTest = template(TEST_TEMPLATE, {
-	interpolate: /<%=([\s\S]+?)%>/g,
+	interpolate: /<\%=([\s\S]+?)%\>/g,
 });
 const testCode = createTest(require('./export-data.js'));
 fs.writeFileSync('./tests/tests.mjs', testCode);
@@ -66,7 +66,7 @@ fs.writeFileSync('./tests/tests.mjs', testCode);
 // src/iso-8859-2.src.mjs -> iso-8859-2.mjs
 const LIB_TEMPLATE = fs.readFileSync('./src/iso-8859-2.src.mjs', 'utf8');
 const createLib = template(LIB_TEMPLATE, {
-	interpolate: /<%=([\s\S]+?)%>/g,
+	interpolate: /<\%=([\s\S]+?)%\>/g,
 });
 const libCode = createLib(require('./export-data.js'));
 fs.writeFileSync('./iso-8859-2.mjs', libCode);
--- tests/tests.src.mjs
+++ tests/tests.src.mjs
@@ -50,6 +50,11 @@ assert.throws(
 	Error,
 	'Mode names are case-insensitive'
 );
+assert.deepStrictEqual(
+	iso88592.encode('\uFFFF', { mode: 'replacement' }),
+	new Uint16Array([0xFFFD]),
+	'Encoding a code point that is invalid for this encoding results in U+FFFD in `replacement` mode'
+);
 
 console.log('Testing `iso88592.decode`…');
 assert.deepStrictEqual(
